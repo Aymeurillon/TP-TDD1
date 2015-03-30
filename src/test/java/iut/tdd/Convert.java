362932b2af08cdf83199034bbfaae6d5f3c72252
Aymeric Meurillon <aymericmeurillon@free.fr>
2015-03-30 15:19:07 +0200

    test3

--- src/test/java/iut/tdd/Convert.java
+++ src/test/java/iut/tdd/Convert.java
@@ -1,42 +1,32 @@
 package iut.tdd;
 
+import java.util.HashMap;
+
 public class Convert {
+	
+	static HashMap<String, String> listeHashMap = new HashMap<String, String>();
+	static {
+		listeHashMap.put("0", "zéro");
+		listeHashMap.put("1", "un");
+		listeHashMap.put("2", "deux");
+		listeHashMap.put("3", "trois");
+		listeHashMap.put("4", "quatre");
+		listeHashMap.put("5", "cinq");
+		listeHashMap.put("6", "six");
+		listeHashMap.put("7", "sept");
+		listeHashMap.put("8", "huit");
+		listeHashMap.put("9", "neuf");
+		listeHashMap.put("10", "dix");
+		listeHashMap.put("11", "onze");
+		listeHashMap.put("12", "douze");
+		listeHashMap.put("13", "treize");
+		listeHashMap.put("14", "quatorze");
+		listeHashMap.put("15", "quinze");
+		listeHashMap.put("16", "seize");
+	}
+	
 	public static String num2text(String input) {
-		if (input.equals("0")){
-			return "zéro";
-		}else if(input.equals("1")){
-			return "un";
-		}else if(input.equals("2")){
-			return "deux";
-		}else if(input.equals("3")){
-			return "trois";
-		}else if(input.equals("4")){
-			return "quatre";
-		}else if(input.equals("5")){
-			return "cinq";
-		}else if(input.equals("6")){
-			return "six";
-		}else if(input.equals("7")){
-			return "sept";
-		}else if(input.equals("8")){
-			return "huit";
-		}else if(input.equals("9")){
-			return "neuf";
-		}else if(input.equals("10")){
-			return "dix";
-		}else if(input.equals("11")){
-			return "onze";
-		}else if(input.equals("12")){
-			return "douze";
-		}else if(input.equals("13")){
-			return "treize";
-		}else if(input.equals("14")){
-			return "quatorze";
-		}else if(input.equals("15")){
-			return "quinze";
-		}else{
-			return "seize";			
-		}
+		return (listeHashMap.get(input));
 	}
 	public static String text2num(String input) {
 		return null;
